09c3442afc49c1a3afbb59429ad92c39ce9b9c7c
MacSwider <maciejswiderbusiness@gmail.com>
2025-01-13 17:22:06 +0100

    wersja_surowa
    -bez wizualizacji
    -sam plik .txt

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def read_mapa(filename):
         map_data = [list(map(int, line.strip().split())) for line in file]  #strip usuwa białe znaki i dzielimy linie na liste stringów
         return map_data
 
+
 def heurestyka(x, y):
     return math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)               #nasza heuretyska
 
@@ -52,55 +53,6 @@ def a_gwiazdka(map_data, start, cel):
 
     raise Exception("Błąd 21 - Brak dostępnej ścieżki")
 
-def wizualizuj_animacje(map_dane, odwiedzone, sciezka, f_score):
-    grid = np.array(map_dane)
-
-    fig, ax = plt.subplots(figsize=(10, 10))                                        #tworzenie wykresu
-    cmap = plt.cm.colors.ListedColormap(['white', 'blue', 'red', 'green', 'gray'])  #mapa_kolorów
-    przedzialy = [0, 1, 2, 3, 4, 6]
-    norm = plt.cm.colors.BoundaryNorm(przedzialy, cmap.N)
-
-    im = ax.imshow(grid, cmap=cmap, norm=norm, extent=[-0.5, grid.shape[1] - 0.5, -0.5, grid.shape[0] - 0.5])
-    # Ustawienia osi i siatki na mapie
-    plt.xticks(np.arange(grid.shape[1]) - 0.5, labels=np.arange(grid.shape[1]))
-    plt.yticks(np.arange(grid.shape[0]) - 0.5, labels=np.arange(grid.shape[0]))
-    plt.grid(True, color='black', linewidth=0.5)
-    plt.title("Obliczanie f_score dla każdego pola")
-
-    all_texts = {}
-    current_step = [0]
-
-    def update(frame):
-        if current_step[0] < len(odwiedzone):                            # Pobieranie współrzędnych odwiedzonego pola
-            x, y = odwiedzone[current_step[0]]
-
-            grid[x][y] = 3                                               # Oznacza odwiedzone pole na mapie
-            im.set_data(grid)
-
-            sasiedzi = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
-            # Wyświetlanie wartości f_score dla sąsiadów
-            for sasiad in sasiedzi:
-                if (0 <= sasiad[0] < grid.shape[0] and 0 <= sasiad[1] < grid.shape[1]
-                        and map_dane[sasiad[0]][sasiad[1]] != 5):
-                    f_value = f_score[sasiad[0]][sasiad[1]]
-                    if f_value != float('inf') and (sasiad[0], sasiad[1]) not in all_texts:
-                        text = ax.text(sasiad[1], grid.shape[0] - 1 - sasiad[0], f"{f_value:.2f}",
-                                       ha='center', va='center', color='black', fontsize=6)
-                        all_texts[(sasiad[0], sasiad[1])] = text
-
-            current_step[0] += 1                                        # Przechodzenie do następnego kroku
-        elif current_step[0] == len(odwiedzone):
-            for x, y in sciezka:
-                grid[x][y] = 2                                          # Oznacza ścieżkę na mapie na czerwono
-            im.set_data(grid)
-            current_step[0] += 1
-
-        return [im] + list(all_texts.values())
-                                                                        #funkcja update
-    ani = animation.FuncAnimation(fig, update, frames=len(odwiedzone) + 1, interval=20, repeat=False) #przy dużej mapie
-    plt.show()                                                                                        #lepiej dać > 50
-    return ani
-
 mapa = 'mapa.txt'
 map_dane = read_mapa(mapa)
 start = (0, 0)
@@ -115,17 +67,16 @@ try:
     if sciezka is not None:
         print("Ścieżka znaleziona")
 
-        # Wyświetlenie mapy z widoczną trasą w terminalu
-        mapa_z_trasa = [row[:] for row in map_dane]  # Tworzymy kopię mapy
+        mapa_z_trasa = [row[:] for row in map_dane]
+
         for x, y in sciezka:
-            mapa_z_trasa[x][y] = 2                  # Zaznaczamy ścieżkę na mapie
+            mapa_z_trasa[x][y] = 2
 
-        print("Mapa w terminalu:")
-        for row in mapa_z_trasa:
-            print(" ".join(map(str, row)))         # Wyświetlamy wiersze mapy w terminalu
+        with open("mapa_z_trasa.txt", "w") as plik:
+            for row in mapa_z_trasa:
+                plik.write(" ".join(map(str, row)) + "\n")
 
-        # Wizualizacja animacji
-        wizualizuj_animacje(map_dane, odwiedzone, sciezka, f_score)
+        print("Mapa została zapisana!")
 
 except Exception as e:
     print(e)
